a5aa758266b12c34461019a84e5b179d3ca117b3
Eoin_Brennan <ebrennan12345@gmail.co.uk>
2022-10-02 19:08:45 +0100

    ansible update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_ranges = ["22", "80", "443"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
